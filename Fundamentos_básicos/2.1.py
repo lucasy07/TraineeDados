f3d2d914aed597bd2a14468a9b422167bf5d090e
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-30 17:30:01 -0300

    Update and rename nulo to 2.1.py

---
+++ Fundamentos_básicos/2.1.py
@@ -0,0 +1,11 @@
+original = "  python para dados  "
+sem_espaços = original.strip()
+maiusculas = original.upper()
+minusculas = original.lower()
+capitalizado = original.capitalize()
+
+print(original)
+print(sem_espaços)
+print(maiusculas)
+print(minusculas)
+print(capitalizado)
