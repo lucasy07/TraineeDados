c6d6d1e23e7d34d7ce160dd376aa93e1fac5bea4
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-05-30 17:49:01 -0300

    Resolução 2.3

---
+++ Fundamentos_básicos/2.3.py
@@ -0,0 +1,10 @@
+data = "2024-03-15"
+
+ano = data[0:4]
+mes = data[5:7]
+dia = data[8:10]
+
+print(f"Ano: {ano}")    
+print(f"Mès: {mes}")
+print(f"Dia: {dia}")
+print(f"Data formatada: {dia}/{mes}/{ano}")
