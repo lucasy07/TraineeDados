a4ab342c226a9fcc6659f4ae7c537c82ae862bad
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-06-03 16:48:52 -0300

    Create 2.2.py

---
+++ Funções_e_Módulos/2.2.py
@@ -0,0 +1,5 @@
+def calcular_media(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+    print(f"A médias das notas {nota1}, {nota2}, {nota3} é: {media:.2f}")
+
+calcular_media(8.5, 7, 9.5)
