b2015574b6dfb38d2cec10267284ca721a04728b
Lucas Yoshimura <lucasyoshimura07@gmail.com>
2026-06-03 17:53:01 -0300

    Create 6.2.py

---
+++ Funções_e_Módulos/6.2.py
@@ -0,0 +1,14 @@
+from random import randint
+import math as m
+
+a = randint(1, 100)
+b = randint(1, 100)
+c = randint(1, 100)
+
+print(f"Números aleatórios gerados por randint(): {a}, {b}, {c}")
+
+d = m.sqrt(64)
+e = m.pow(2, 10)
+
+print(f"Raiz de 64: {d}")
+print(f"2 elevado a 10: {e}")
